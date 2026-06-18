951ef3903bd9584f9100c804631f1117b1cbb49a
feng-y-u <1834677757@qq.com>
2026-06-18 12:58:21 +0800

    fix: address code quality issues for Tauri init

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,8 +1,11 @@
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+mod commands;
+
 fn main() {
     tauri::Builder::default()
         .plugin(tauri_plugin_store::Builder::new().build())
+        .invoke_handler(tauri::generate_handler![])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
